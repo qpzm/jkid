0dd8156b1b023ad275ef1dc3a8981adfd2f78552
qpzm <lhm1442@gmail.com>
2020-11-09 04:10:52 +0900

    Solve Dateformat deserialization

--- src/main/kotlin/exercise/DateFormat.kt
+++ src/main/kotlin/exercise/DateFormat.kt
@@ -11,8 +11,7 @@ class DateSerializer(format: String) : ValueSerializer<Date> {
     private val simpleDateFormat = SimpleDateFormat(format)
 
     override fun fromJsonValue(jsonValue: Any?): Date {
-        if (jsonValue !is Date) throw JKidException("Expected date, was: $jsonValue")
-        return jsonValue
+        return simpleDateFormat.parse(jsonValue as String)
     }
 
     override fun toJsonValue(value: Date): String = simpleDateFormat.format(value)
